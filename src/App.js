622aa1621dab32164d0281d909b463f10df49ee8
shawnlknight <shawn.leberknight@gmail.com>
2017-10-02 15:50:07 -0400

    Removed more stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 // @flow
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
-const addFnc = (a: number, b: number) => a + b
-
-addFnc(1, 2)
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import add from './addFunction/index';
 
 const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1 className='App-title'>Welcome to React</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Using Flow with React</h1>
       </header>
-      <p className='App-intro'>
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      <div>
+        <h4>Checking number types</h4>
+        <p>
+          The sum of 2 + 2 is <strong>{add(2, 2)}</strong>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/addFunction/add.test.js
@@ -0,0 +1,5 @@
+import add from './'
+
+it('adds 2 plus 2', () => {
+  expect(add(1, 2)).toEqual(3)
+})
